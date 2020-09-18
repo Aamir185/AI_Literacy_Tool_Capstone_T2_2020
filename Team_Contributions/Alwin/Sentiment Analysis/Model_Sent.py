92c2fe54250c2cee5d03a8f5af2c782c6e421661
Alwin Antony <al.alwinantony@gmail.com>
2020-09-18 11:37:31 +1000

    Modified sentiment analysis scripts and model

--- Team_Contributions/Alwin/Sentiment Analysis/Model_Sent.py
+++ Team_Contributions/Alwin/Sentiment Analysis/Model_Sent.py
@@ -8,6 +8,7 @@ import pickle
 def remove_noise(tweet_tokens, stop_words = ()):
 
     cleaned_tokens = []
+    t = []
 
     for token, tag in pos_tag(tweet_tokens):
         token = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|'\
@@ -23,18 +24,21 @@ def remove_noise(tweet_tokens, stop_words = ()):
 
         lemmatizer = WordNetLemmatizer()
         token = lemmatizer.lemmatize(token, pos)
+        t.append(token.lower())
 
         if len(token) > 0 and token not in string.punctuation and token.lower() not in stop_words:
             cleaned_tokens.append(token.lower())
-    return cleaned_tokens
+
+    return cleaned_tokens, t
+
 
 
 if __name__ == "__main__":
 
     #custom_tweet = "I ordered just once from TerribleCo, they screwed up, never used the app again."
     custom_tweet = "I ordered just once from TerribleCo, it was a good experince"
-    custom_tokens = remove_noise(word_tokenize(custom_tweet))
+    custom_tokens,r = remove_noise(word_tokenize(custom_tweet))
     loaded_model = pickle.load(open("sent_model.sav", 'rb'))
     result = loaded_model.classify(dict([token, True] for token in custom_tokens))
-    print(custom_tweet,"Sentiment: ",result)
+    print(r)
     #print(dict([token, True] for token in custom_tokens))
